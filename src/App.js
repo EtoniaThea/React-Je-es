b9a54aaa8c7c6e1e4ae1fe1f97c61b670d6f51b7
EtoniaThea <waskithocito.a@gmail.com>
2022-12-14 11:20:13 +0700

    Tugas Reak Je'es

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Putar<code>Bola</code>bulu
         </p>
         <a
           className="App-link"
